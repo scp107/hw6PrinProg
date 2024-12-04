62d8c29c9339fe8df7fba7b9bc5ffed4f3646c36
Ari Lei <69610637+scp107@users.noreply.github.com>
2024-12-03 21:42:02 -0500

    Update getnthdigit.ml

--- getnthdigit.ml
+++ getnthdigit.ml
@@ -1,10 +1,10 @@
 let getnthdigit num n =
-  let rec helper num index length =
+  let rec helper num n length =
     match num with
     | 0 -> -1  
-    | _ when index = length -> num mod 10  
-    | _ -> helper (num / 10) (index + 1) length  
+    | _ when length = n -> num mod 10  
+    | _ -> helper (num / 10) n (length + 1)  
   in
-  let length = String.length (string_of_int num) in
-  if n > length || n <= 0 then -1 
-  else helper num 1 (length - n + 1)
+  let num_length = String.length (string_of_int num) in
+  if n > num_length || n <= 0 then -1  
+  else helper num n 1  
