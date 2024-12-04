cf3ca1fa6f445fbdd781e3fb69bb63e38983a381
Ari Lei <69610637+scp107@users.noreply.github.com>
2024-12-03 21:31:24 -0500

    Update getnthdigit.ml

--- getnthdigit.ml
+++ getnthdigit.ml
@@ -1,6 +1,10 @@
-let rec getnthdigit num n =
-  let digits = string_of_int num in  
-  if n <= 0 || n > String.length digits then
-    failwith "Invalid digit position" 
-  else
-    int_of_char (String.get digits (n - 1)) - int_of_char '0'  
+let getnthdigit num n =
+  let rec helper num index length =
+    match num with
+    | 0 -> -1  
+    | _ when index = length -> num mod 10  
+    | _ -> helper (num / 10) (index + 1) length  
+  in
+  let length = String.length (string_of_int num) in
+  if n > length || n <= 0 then -1 
+  else helper num 1 (length - n + 1)
