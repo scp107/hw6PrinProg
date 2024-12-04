69e012e2d3a62c06ddbc962bf22be400bd0662f0
Ari Lei <69610637+scp107@users.noreply.github.com>
2024-12-03 21:19:45 -0500

    Update maxAbsoluteVal.ml

--- maxAbsoluteVal.ml
+++ maxAbsoluteVal.ml
@@ -1,2 +1,9 @@
-let maxAbsoluteVal l =
-  List.fold_left (fun acc x -> max acc (abs x)) 0 l
+let maxAbsoluteVal lst =
+  let rec helper lst current_max =
+    match lst with
+    | [] -> current_max  
+    | x :: xs ->
+        let abs_x = abs x in  
+        helper xs (max abs_x current_max)  
+  in
+  helper lst 0  
